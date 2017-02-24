5ae63e1b0959391110a07c0c02a1ffa25d154614
Traci Williams <ug3000@gmail.com>
2017-02-24 15:07:16 -0600

    added an event listener and new function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
-window.onload = function(){
-	console.log("javascript file loaded");
-};
+// window.onload = function(){
+// 	console.log("javascript file loaded");
+// };
 
 /*PSUEDO CODE --- 
 need to make variables for the form, the input and list item
@@ -9,17 +9,35 @@ need to select the value that is inputted
 need to select the ordered list
 when someone clicks on the submit button it should 
 put the value that is inputted into the ordered list
-display the value on the page*/
+display the value on the page
+
+
+First create an LI node,
+then create a Text node,
+then append the Text node to the LI node.
+Finally append the LI node to the list.
+
+
+
+*/
 
 var taskForm = document.getElementById("taskForm"); //form
-var taskInput = document.getElementsByTagName("input")[0].value; //input
-var newItem = document.createElement("li"); //list item
+var taskList = document.getElementById("taskList"); //orderded list
 var button = document.getElementsByTagName("button"); //button
 
-function submitTask() {
-	newItem.innerhtml = taskInput;
-	taskForm.appendChild(newItem);
-};
 
-submitTask();
-// button.addEventListener("click", submitTask);
+function submitTask(){
+	var newItem = document.createElement("li"); //creates an li
+	var taskInput = document.getElementById("user_task").value //get the value from the input field
+ 	newItem.appendChild(taskInput);//appends value to the li  
+ 	document.getElementById("taskList").appendChild(newItem);//appends to list unordered list
+ };
+
+
+// document.getElementById("subBtn").addEventListener("click", function(){
+//     document.getElementById("taskList").innerHTML = "Hello World";
+// });
+
+document.getElementById("subBtn").addEventListener("click", submitTask);
+
+
