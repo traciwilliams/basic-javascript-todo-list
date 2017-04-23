52102211471cc2818e55a6b7f055052e8b99b017
Traci Williams <ug3000@gmail.com>
2017-04-23 01:04:57 -0500

    modified objects

--- scripts/app.js
+++ scripts/app.js
@@ -2,64 +2,12 @@ window.onload = function(){
 	console.log("javascript file loaded");
 };
 
-/*PSUEDO CODE --- 
-need to make variables for the form, the input and list item
-need to select the form
-need to select the value that is inputted
-need to select the div
-when someone clicks on the submit button it should 
-put the value that is inputted into the div 
-(First create an LI node,
-then get the value
-then append the value to the LI node.
-Finally append the LI to the list.)
-display the value on the page
-
-*/
-
-// var taskForm = document.getElementById("taskForm"); //form
-// var button = document.getElementsByTagName("button"); //button
-//  document.getElementById("taskList").appendChild(newItem);//appends to list unordered list
-// document.getElementById("subBtn").addEventListener("click", submitTask);
 
 
 var btnNew = document.getElementById("subBtn");//button
 
 
-// function submitTask() {
-// 	var taskInput = document.getElementsByTagName("input")[0].value;//get the value from the input field	
-// 	document.getElementsByTagName("input")[0].value = ''; //this clears the field for new items to be added
 
-// 	var newItem = document.createElement("li"); //ceates an li
-// 	newItem.innerHTML = taskInput;//makes a list item with the value entered
-
-// 	document.getElementById("taskList").appendChild(newItem);//appends new item to the unordered list  
-	
-// 	};
-	
-// 	document.getElementsByTagName("button")[0].addEventListener("click", submitTask);//calls submitTask function on the button click
-
-	
-
-/*function submitTask(e) {
-	//var taskInput = document.getElementsByTagName("input")[0].value;//get the value from the input field	
- 	//document.getElementsByTagName("input")[0].value = ''; //this clears the field for new items to be added
-
- 	"use strict";
-    e.preventDefault();
-
-    var taskList = document.getElementById("taskList");
- 	var newItem = document.createElement("li"); //creates an li
- 	var taskInput = document.getElementsByTagName("input")[0].value;//get the value from the input field
- 	var t = document.createTextNode(taskInput);//makes a list item with the value entered
- 	newItem.appendChild(t);
-
- 	taskList.innerHTML += '<li>' + taskInput + '</li>';
-};
-
-
-document.getElementsByTagName("button")[0].addEventListener("click", submitTask);//calls submitTask function on the button click
-*/
 
 
 //making our todoList object
@@ -88,11 +36,8 @@ var todoList = {
 				todoText: todoText,
 				completed: false
 			});
-			this.displayToDos();
+			
 		},
-
-
-
 
 		addComment: function(todoComment) {
 			this.todoComment.push ({
@@ -102,25 +47,18 @@ var todoList = {
 		},
 
 
-
-
-
-
-
-
-
 		changeToDo: function(position, todoText) {
 			this.todos[position].todoText = todoText;
-			this.displayToDos();
+			
 		},
 		deleteToDo: function(position) {
 			this.todos.splice(position, 1);
-			this.displayToDos();
+			
 		},
 		toggleCompleted: function(position) {
 			var todo = this.todos[position];
 			todo.completed = !todo.completed;
-			this.displayToDos();
+			
 		},
 		toggleAll: function() {
 			var totalTodos = this.todos.length;
@@ -140,7 +78,7 @@ var todoList = {
 				this.todos[i].completed = true;
 			}
 		}
-		this.displayToDos();
+		
   	}
   };		
 
@@ -228,9 +166,9 @@ var view = {
 
 
 			if (todo.completed === true) {
-				todoTextWithCompletion = '(x)' + todo.todoText;
+				todoTextWithCompletion = '(x) ' + todo.todoText;
 			} else {
-				todoTextWithCompletion = '( )' + todo.todoText;
+				todoTextWithCompletion = '( ) ' + todo.todoText;
 			}
 
 			todosLi.textContent = todoTextWithCompletion;
